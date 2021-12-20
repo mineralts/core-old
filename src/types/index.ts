d19d49b9ecd6df7ea944b4e5bbd341086bec142d
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-20 11:31:08 +0100

    :tada: Initial commit

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @mineralts/index.ts
+ *
+ * (c) Parmantier Baptiste
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */

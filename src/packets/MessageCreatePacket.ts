e0176239060f8173641f01b0fbd22efd4748bcf9
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-27 23:17:34 +0100

    :construction: Work in progress

---
+++ src/packets/MessageCreatePacket.ts
@@ -0,0 +1,26 @@
+import { TextChannel } from '@mineralts/api'
+import { Assembler } from '@mineralts/assembler'
+import Packet from '../entities/Packet'
+import MessageBuilder from '../builders/MessageBuilder'
+
+export default class MessageCreatePacket extends Packet {
+  public packetType = 'MESSAGE_CREATE'
+
+  public async handle (assembler: Assembler, payload: any) {
+    const guild = assembler.application.client.guilds.cache.get(payload.guild_id)
+    const channel = guild?.channels.cache.get(payload.channel_id) as TextChannel
+
+    console.log('channel', channel)
+
+    const messageBuilder = new MessageBuilder(assembler.application.client)
+    const message = messageBuilder.build(payload)
+
+    if (channel) {
+      channel.lastMessageId = message.id
+      channel.lastMessage = message
+      channel.messages.cache.set(message.id, message)
+    }
+
+    assembler.eventListener.emit('messageCreate', message)
+  }
+}

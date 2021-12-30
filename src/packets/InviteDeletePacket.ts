decc53b699a8e04c710217a0c1d12f12496d765d
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-30 23:37:28 +0100

    :construction: Implement InviteDeletePackage

---
+++ src/packets/InviteDeletePacket.ts
@@ -0,0 +1,19 @@
+import { Assembler } from '@mineralts/assembler'
+import Packet from '../entities/Packet'
+
+export default class InviteDeletePacket extends Packet {
+  public packetType = 'INVITE_DELETE'
+
+  public async handle (assembler: Assembler, payload: any) {
+    const guild = assembler.application.client.guilds.cache.get(payload.guild_id)
+    const invite = guild!.invites.cache.get(payload.code)
+
+    if(!invite) {
+      return
+    }
+
+    assembler.eventListener.emit('inviteDelete', invite)
+
+    guild?.invites.cache.delete(invite.code)
+  }
+}

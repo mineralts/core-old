81f5f62b237391c5c13782bd0aab9298a03bdcd4
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-14 08:56:54 +0100

    :construction: Work in progress

--- src/standalone/Ignitor.ts
+++ src/standalone/Ignitor.ts
@@ -16,14 +16,17 @@ export default class Ignitor {
     const forgeFile = path.join('node_modules', '@mineralts', 'core', 'build', 'src', 'forge')
     const argv = process.argv.slice(2)
     const stringArgs = argv.join(' ')
-    const command = `ts-node ${forgeFile} ${stringArgs}`
+
+    const tsnode = path.join(process.cwd(), 'node_modules', 'ts-node', 'dist', 'bin.js')
+    const command = `node ${tsnode} ${forgeFile} ${stringArgs}`
 
     try {
-      const { stdout } = await exec(command, {
-        cwd: process.cwd()
+      const { stdout, stderr } = await exec(command, {
+        cwd: process.cwd(),
       })
 
       stdout?.pipe(process.stdout)
+      stderr?.pipe(process.stderr)
     } catch (e) {
       console.log(e)
     }

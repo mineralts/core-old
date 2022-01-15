61097f5dd9af492589b95fba19d3dca1262aef59
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-15 17:00:54 +0100

    :construction: Work in progress

--- src/commands/MakeEvent.ts
+++ src/commands/MakeEvent.ts
@@ -10,20 +10,46 @@
 
 import { Command } from '@mineralts/forge'
 import Generator from '../Generator'
+import { prompt } from 'enquirer'
+import { clientEvents } from '@mineralts/api'
+import path from 'path'
 
 export default class GenerateManifest extends Command {
   public static commandName = 'make:event'
   public static description = 'Make a new event class'
 
-  public static settings = {
-    loadApp: true
-  }
-
   public async run (...args: string[]): Promise<void> {
     const generator = new Generator(this.logger)
-    await generator.write('event', args)
-    await generator.buildFolders()
+    await generator.loadFolders(path.join(process.cwd(), 'src'))
 
-    await generator.writeFile()
+    const location = {
+      type: 'autocomplete',
+      name: 'filename',
+      message: 'Please define path location',
+      limit: 3,
+      choices: generator.getFolders()
+    }
+
+    const filename = {
+      type: 'input',
+      name: 'filename',
+      message: 'Please define a name for your file'
+    }
+
+    const eventType = {
+      type: 'autocomplete',
+      name: 'event',
+      message: 'Please select an event',
+      limit: 5,
+      choices: clientEvents
+    }
+
+    const answers = await prompt([filename, location, eventType]) as { filename, location, eventType }
+    // generator.setFilename(answers.filename)
+
+    // await generator.write('event', args)
+    // await generator.buildFolders()
+    //
+    // await generator.writeFile()
   }
 }

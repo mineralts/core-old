4e225aa9ac796ab0c4247804aa7e9c65d59a766b
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-15 16:59:53 +0100

    :construction: Work in progress

--- src/standalone/Ignitor.ts
+++ src/standalone/Ignitor.ts
@@ -10,25 +10,65 @@
 
 import { exec } from 'child_process'
 import path from 'path'
+import Kernel from '../Kernel'
+import Logger from '@mineralts/logger'
+import Environment from '../Environment'
 
 export default class Ignitor {
+  private logger: Logger = new Logger
+  private environment: Environment = new Environment()
+
   public async forge () {
+    const [commandName, ...args] = process.argv.slice(2)
+
+    this.environment.registerEnvironment()
+
+    if (commandName === 'generate:manifest' || commandName === 'help' || !commandName) {
+      await this.execTypescript(commandName, ...args)
+    } else {
+      await this.execJavascript(commandName, ...args)
+    }
+  }
+
+  private async execTypescript (commandName: string, ...args: string[]) {
     const forgeFile = path.join('node_modules', '@mineralts', 'core', 'build', 'src', 'forge')
-    const argv = process.argv.slice(2)
-    const stringArgs = argv.join(' ')
+    const stringArgs = args.slice(1).join(' ')
 
     const tsnode = path.join(process.cwd(), 'node_modules', 'ts-node', 'dist', 'bin.js')
     const command = `node ${tsnode} ${forgeFile} ${stringArgs}`
 
-    try {
-      const { stdout, stderr } = await exec(command, {
-        cwd: process.cwd(),
-      })
+    const { stdout, stderr, stdin } = await exec(command, {
+      cwd: process.cwd(),
+      env: {
+        TOKEN: this.environment.cache.get<string>('TOKEN'),
+        COMMAND_NAME: commandName,
+        ARGS: stringArgs
+      }
+    })
 
-      stdout?.pipe(process.stdout)
-      stderr?.pipe(process.stderr)
-    } catch (e) {
-      console.log(e)
+    stdout?.pipe(process.stdout)
+    stderr?.pipe(process.stderr)
+    process.stdin.pipe(stdin!)
+  }
+
+  private async execJavascript (commandName: string, ...args: string[]) {
+    const token = this.environment.cache.get<string>('TOKEN')
+
+    if (!token) {
+      this.logger.fatal('Token was not provided')
+      process.exit(1)
     }
+
+    const kernel = new Kernel(token)
+    await kernel.application.registerCliCommands()
+
+    const command = kernel.application.commands.get(commandName)
+
+    if (!command) {
+      this.logger.error('Command not found.')
+      return
+    }
+
+    await command.run(...args)
   }
 }
